51dc0837a19a8c3008e26e2701fbab0d51b589e8
Duvan De Koker <DuvanD@discovery.co.za>
2018-12-29 09:17:54 +0100

    - Added rough math function in kotlin. - Use // @! Deesha Singh to see my comments. - Deesha - please get this functional modified or similar.

--- app/src/main/java/com/aconno/sensorics/SensoricsApplication.kt
+++ app/src/main/java/com/aconno/sensorics/SensoricsApplication.kt
@@ -1,6 +1,7 @@
 package com.aconno.sensorics
 
 import android.app.Application
+import android.hardware.Sensor
 import com.aconno.sensorics.dagger.application.AppComponent
 import com.aconno.sensorics.dagger.application.AppModule
 import com.aconno.sensorics.dagger.application.DaggerAppComponent
@@ -14,6 +15,7 @@ class SensoricsApplication : Application() {
 
     lateinit var appComponent: AppComponent
 
+
     override fun onCreate() {
         super.onCreate()
         if (LeakCanary.isInAnalyzerProcess(this)) {
@@ -24,6 +26,29 @@ class SensoricsApplication : Application() {
         LeakCanary.install(this)
         Timber.plant(Timber.DebugTree())
         Fabric.with(this, Crashlytics())
+
+
+//        this.sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//
+//        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.let {
+//            this.accelerometer = it
+//        }
+//
+//        sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)?.let {
+//            this.gravity = it
+//        }
+//
+//        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.let {
+//            this.gyroscope = it
+//        }
+//
+//        sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)?.let {
+//            this.linearAcceleration = it
+//        }
+//
+//        sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)?.let {
+//            this.rotationVector = it
+//        }
 
         val mapper = AdvertisementFormatMapper()
         val reader = AdvertisementFormatReader()
